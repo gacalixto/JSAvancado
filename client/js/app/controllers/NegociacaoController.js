a434705e9d02df99de7fe044abd71d1ce995a181
Gabriel <gabrielcalixto9@gmail.com>
2018-03-11 22:55:28 -0300

    adicionando helper para datas e strings pré organizadas para mostrar datas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,24 +4,22 @@ class NegociacaoController{
 		let $ = document.querySelector.bind(document);
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
-		this._inputValor = $("#valor");
+		this._inputValor = $("#valor"),
+		this._listaNegociacoes = new ListaNegociacoes();
 
 	}
 
 	adiciona(event){
 		event.preventDefault();
-		let data = new Date(...this._inputData.value
-			.split('-')
-			.map((item,indice)=>  item-indice%2));
+		 
 
 		let negociacao = new Negociacao(
-			data,
+			DateHelper.textoParaData(this._inputData.value),
 			this._inputQuantidade.value,
 			this._inputValor.value
-			)
-		let diaMesAno = negociacao.data.getDate() + '/' + (negociacao.data.getMonth()+1) + '/' + negociacao.data.getFullYear();
-
-		console.log(diaMesAno);
+			);
+		this._listaNegociacoes.adiciona(negociacao);
+		console.log(this._listaNegociacoes.negociacoes);
 	}
 
 
