bc20cd80dea4606915acd6b9efbcec617ffb03fb
Gabriel <gabrielcalixto9@gmail.com>
2018-03-12 10:42:38 -0300

    adicionando view de negociacoes

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -5,14 +5,15 @@ class DateHelper{
 	}
 
 	static textoParaData(texto){
+			if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
+			throw new Error("Deve estar aaaa-mm-dd");
 		return new Date(...texto.split('-')
 			.map((item,indice)=>  item-indice%2));
 
 	}
 
 	static dataParaTexto(data){
-		if(!/\d{4}-\d{2}-\d{2}/.test(texto))
-			throw new Error("Deve estar aaaa-mm-dd");
+	
 		return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
 
 				 
