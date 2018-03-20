86a36bddc35ad80e92759017b2ea1e85508e9d37
Gabriel <gabrielcalixto9@gmail.com>
2018-03-20 11:38:42 -0300

    adiconando classe bind de proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,15 +4,20 @@ class NegociacaoController{
 		let $ = document.querySelector.bind(document);
 		this._inputData = $("#data");
 		this._inputQuantidade = $("#quantidade");
-		this._inputValor = $("#valor"),
-		/*this._listaNegociacoes = new ListaNegociacoes(model=>
-			this._negociacoesView.update(model));*/
+		this._inputValor = $("#valor");
 		this._negociacoesView = new NegociacoesView($('#negociacoesView'));
 
-		this._negociacoesView.update(this._listaNegociacoes);
-		this._mensagem = new Mensagem();
+		this._listaNegociacoes = new Bind(
+			new ListaNegociacoes(),
+			this._negociacoesView,
+			['adiciona','esvazia']
+		);
+
 		this._mensagemView = new MensagemView($("#mensagemView"));
-		this._mensagemView.update(this._mensagem);
+
+		this._mensagem = new Bind(new Mensagem(),
+		this._mensagemView,
+		['texto']);
 
 	}
 
@@ -22,7 +27,6 @@ class NegociacaoController{
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
 
 		this._mensagem.texto = "Negociação adicionada";
-		this._mensagemView.update(this._mensagem);
 		this._limpaFormulario();
 		console.log(this._listaNegociacoes.negociacoes);
 	}
@@ -31,8 +35,6 @@ class NegociacaoController{
 		this._listaNegociacoes.esvazia();
 
 		this._mensagem.texto = "Negociações apagadas com sucesso";
-		this._mensagemView.update(this._mensagem);
-
 
 	}
 	_criaNegociacao(){
