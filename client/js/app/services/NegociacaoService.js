1675f6d2a76f2accd96947eabe7018dd8d1c869b
Gabriel <gabrielcalixto9@gmail.com>
2018-04-10 16:13:14 -0300

    comm

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -3,6 +3,24 @@ class NegociacaoService{
     constructor(){
         this._http = new HttpService();
     }
+    obterNegociacoes() {
+
+        return Promise.all([
+            this.obterNegociacoesDaSemana(),
+            this.obterNegociacoesDaSemanaAnterior(),
+            this.obterNegociacoesDaSemanaRetrasada()
+        ]).then(periodos => {
+
+            let negociacoes = periodos
+                .reduce((dados, periodo) => dados.concat(periodo), []);
+
+            return negociacoes;
+
+        }).catch(erro => {
+            throw new Error(erro);
+        });
+
+    } 
     obterNegociacoesDaSemana() {
 
         return new Promise((resolve, reject) => {
